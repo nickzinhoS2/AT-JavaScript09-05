c1caaee1aa606028089429b498889c961287c66a
nickzinhoS2 <102757773+nickzinhoS2@users.noreply.github.com>
2022-05-09 15:30:37 -0300

    Add files via upload

---
+++ arraySeparar.js
@@ -0,0 +1,7 @@
+const chamada = ['Ray', 'Ruan', 'xablau', 'maridasDores', 'felipe', 'Danilo', 'Daniel', 'Erico']
+console.log(chamada)
+
+let grupo1 = chamada.slice (0, chamada.lenght/2) 
+let grupo2 = chamada.slice (chamada.length/2)
+console.log(grupo1)
+console.log(grupo2)
---
+++ arraySubstituir.js
@@ -0,0 +1,4 @@
+let chamada = ['leo', 'davi', 'vk', 'lai']
+chamada.splice(1,2, 'Nicolly')
+
+console.log(chamada)
